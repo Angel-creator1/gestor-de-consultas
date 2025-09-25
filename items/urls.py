7e03d8f1a2e95c2fe11f9d4d48580b1abb0465cc
Angel-creator1 <gamadavidjuanluis.ag@gmail.com>
2025-09-25 18:33:13 -0500

    ya con items

--- items/urls.py
+++ items/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('items/', views.variable_list, name='itemList'),
-    path('itemscreate/', csrf_exempt(views.variable_create), name='itemCreate'),
+    path('items/', views.item_list, name='itemList'),
+    path('itemcreate/', csrf_exempt(views.item_create), name='itemCreate'),
 ]
